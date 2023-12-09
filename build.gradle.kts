5d30e695374bcbfe79ec89e6c2fd5913771c96b3
sawafrolov <frolov@devs-vault.com>
2023-12-09 07:37:23 +0400

    Основа проекта Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.1.2"
+    id("io.spring.dependency-management") version "1.1.2"
+}
+
+group = "com.github.sawafrolov"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-rest")
+
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
