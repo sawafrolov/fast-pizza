9be8da24964e653073cfb0d9be85d48941457e4d
sawafrolov <frolov@devs-vault.com>
2023-12-10 08:43:10 +0400

    Main class

---
+++ src/main/java/com/github/sawafrolov/fastpizza/FastPizzaApplication.java
@@ -0,0 +1,12 @@
+package com.github.sawafrolov.fastpizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastPizzaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastPizzaApplication.class, args);
+    }
+}
