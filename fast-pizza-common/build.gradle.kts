a6854866ec1c81650a2b5863f42814d3b34e9f20
sawafrolov <frolov@devs-vault.com>
2024-12-15 16:07:32 +0400

    Common

---
+++ fast-pizza-common/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.kotlin.exposed.core)
+    implementation(libs.kotlin.exposed.jdbc)
+    implementation(libs.kotlin.exposed.dao)
+    implementation(libs.kotlin.exposed.sql)
+    implementation(libs.kotlin.exposed.java.time)
+}
