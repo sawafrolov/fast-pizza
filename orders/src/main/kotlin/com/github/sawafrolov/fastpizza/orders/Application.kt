33faf05d2b39bb7fbccfbf9a6d160731109e1705
sawafrolov <frolov@devs-vault.com>
2024-12-19 18:08:41 +0400

    JSON Serialization

---
+++ orders/src/main/kotlin/com/github/sawafrolov/fastpizza/orders/Application.kt
@@ -0,0 +1,11 @@
+package com.github.sawafrolov.fastpizza.orders
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+}
---
+++ orders/src/main/kotlin/com/github/sawafrolov/fastpizza/orders/Serialization.kt
@@ -0,0 +1,11 @@
+package com.github.sawafrolov.fastpizza.orders
+
+import io.ktor.serialization.kotlinx.json.json
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+
+fun Application.configureSerialization() {
+    install(ContentNegotiation) {
+        json()
+    }
+}
