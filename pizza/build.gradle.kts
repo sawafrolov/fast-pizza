d0fddf510e80ea02504b3ab864b91f95038e6d86
sawafrolov <frolov@devs-vault.com>
2025-02-01 18:26:09 +0400

    Сервисы

---
+++ pizza/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ktor)
+}
+
+application {
+    mainClass = "io.ktor.server.netty.EngineMain"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+    implementation(libs.ktor.server.core.jvm)
+    implementation(libs.ktor.server.auth.jvm)
+    implementation(libs.ktor.server.netty.jvm)
+    implementation(libs.ktor.server.config.yaml.jvm)
+    implementation(libs.ktor.server.resources.jvm)
+    implementation(libs.ktor.server.host.common.jvm)
+    implementation(libs.ktor.server.status.pages.jvm)
+    implementation(libs.ktor.server.default.headers.jvm)
+    implementation(libs.ktor.server.request.validation)
+    implementation(libs.ktor.server.content.negotiation.jvm)
+    implementation(libs.ktor.serialization.kotlinx.json)
+
+    implementation(libs.ktor.server.openapi)
+    implementation(libs.ktor.server.swagger.jvm)
+
+    implementation(libs.koin.core)
+    implementation(libs.koin.ktor)
+    implementation(libs.koin.logger.slf4j)
+
+    implementation(libs.kotlin.exposed.core)
+    implementation(libs.kotlin.exposed.jdbc)
+    implementation(libs.kotlin.exposed.dao)
+
+    implementation(project(":fast-pizza-common"))
+
+    implementation(libs.logback)
+    implementation(libs.postgresql)
+    implementation(libs.jakarta.validation.api)
+    implementation(libs.hibernate.validator.engine)
+    kapt(libs.hibernate.validator.annotation.processor)
+
+    testImplementation(libs.mockk)
+    testImplementation(libs.kotlin.test.junit)
+    testImplementation(libs.ktor.server.test.host.jvm)
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,9 @@
 rootProject.name = "fast-pizza"
 
 include("fast-pizza-common")
+include("customers")
 include("orders")
+include("pizza")
 
 dependencyResolutionManagement {
     versionCatalogs {
