c8cb0c9e64262ac312fd72d77009f9061c95f713
sawafrolov <frolov@devs-vault.com>
2025-02-14 04:01:29 +0400

    DRAFT

--- iam-proxy/src/main/kotlin/com/github/sawafrolov/fastpizza/iamproxy/config/Koin.kt
+++ iam-proxy/src/main/kotlin/com/github/sawafrolov/fastpizza/iamproxy/config/Koin.kt
@@ -1,12 +1,10 @@
 package com.github.sawafrolov.fastpizza.iamproxy.config
 
+import com.github.sawafrolov.fastpizza.common.util.validation.validator
 import com.github.sawafrolov.fastpizza.iamproxy.services.FakeIamService
 import com.github.sawafrolov.fastpizza.iamproxy.services.IamService
-import com.github.sawafrolov.fastpizza.starter.parameterMessageInterpolator
-import com.github.sawafrolov.fastpizza.starter.validator
 import io.ktor.server.application.*
 import jakarta.validation.Validator
-import org.hibernate.validator.messageinterpolation.ParameterMessageInterpolator
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
@@ -14,12 +12,8 @@ import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
 
 val beans = module {
-    single<ParameterMessageInterpolator> {
-        parameterMessageInterpolator()
-    }
-
     single<Validator> {
-        (parameterMessageInterpolator: ParameterMessageInterpolator) -> validator(parameterMessageInterpolator)
+        validator()
     }
 
     singleOf(::FakeIamService) {
