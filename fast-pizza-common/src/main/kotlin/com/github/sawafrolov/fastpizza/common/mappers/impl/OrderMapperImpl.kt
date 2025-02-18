78fd881fa32a579d18333ce3ea51a05058af83fc
sawafrolov <frolov@devs-vault.com>
2025-02-18 12:20:37 +0400

    Доработки

--- fast-pizza-common/src/main/kotlin/com/github/sawafrolov/fastpizza/common/mappers/impl/OrderMapperImpl.kt
+++ fast-pizza-common/src/main/kotlin/com/github/sawafrolov/fastpizza/common/mappers/impl/OrderMapperImpl.kt
@@ -1,5 +1,6 @@
 package com.github.sawafrolov.fastpizza.common.mappers.impl
 
+import com.github.sawafrolov.fastpizza.common.dto.order.OrderCreateDto
 import com.github.sawafrolov.fastpizza.common.dto.order.OrderStatus
 import com.github.sawafrolov.fastpizza.common.dto.order.OrderViewDto
 import com.github.sawafrolov.fastpizza.common.entities.CustomerEntity
@@ -13,7 +14,17 @@ class OrderMapperImpl(
     private val customerMapper: CustomerMapper,
     private val pizzaMapper: PizzaMapper
 ): OrderMapper {
-    
+
+    override fun mapToEntity(orderCreateDto: OrderCreateDto): OrderEntity =
+        OrderEntity.new {
+            customer = orderCreateDto.customerId
+            cart = orderCreateDto.cart
+            address = orderCreateDto.address
+            totalWeight = orderCreateDto.totalWeight
+            totalPrice = orderCreateDto.totalPrice
+            status = OrderStatus.ACCEPTED.name
+        }
+
     override fun mapToViewDto(
         orderEntity: OrderEntity,
         customerEntity: CustomerEntity,
